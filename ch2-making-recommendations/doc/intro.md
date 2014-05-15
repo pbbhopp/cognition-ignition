fda4b2665167886d2aaf38ae2d279f814889ffc0
Thane Vo <vothane@gmail.com>
2014-05-15 14:11:53 -0600

    add first temporary TDD test for finding similarity scores by Euclidean distance

---
+++ ch2-making-recommendations/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ch2-making-recommendations
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ ch2-making-recommendations/src/ch2_making_recommendations/core.clj
@@ -0,0 +1,10 @@
+(ns ch2-making-recommendations.core)
+
+(defn sim-distance
+  "Determines how similar people are in their tastes. You do this by comparing
+   each person with every other person and calculating a similarity score. 
+   Calculating similarity scores: Euclidean distance."
+  [prefs person1 person2]
+
+
+)
