24a958a5cbe2278210fe1fd46bc19a29e86573d0
Thane Vo <vothane@gmail.com>
2014-05-15 15:29:02 -0600

    add let block

--- ch2-making-recommendations/src/ch2_making_recommendations/core.clj
+++ ch2-making-recommendations/src/ch2_making_recommendations/core.clj
@@ -6,9 +6,10 @@
    each person with every other person and calculating a similarity score. 
    Calculating similarity scores: Euclidean distance."
   [prefs people]
-  (reduce
-    (fn [dict movie] (assoc dict movie 1)) 
-    {}
-    (first (apply clojure.set/intersection 
-      (map (fn [person] (hash-set (keys (get prefs person)))) people))))
+  (let [sim-liked-movies (reduce
+                           (fn [dict movie] (assoc dict movie 1)) 
+                           {}
+                           (first (apply clojure.set/intersection 
+                             (map (fn [person] (hash-set (keys (get prefs person)))) people))))]
+    sim-liked-movies)
 )
