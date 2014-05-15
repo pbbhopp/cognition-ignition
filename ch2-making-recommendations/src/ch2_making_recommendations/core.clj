f625a3f2c17cde729654d26645cca8cc8e49aeb2
Thane Vo <vothane@gmail.com>
2014-05-15 16:21:55 -0600

    pass temporary TDD test for finding similarity scores by Euclidean distance with sum of squares

--- ch2-making-recommendations/src/ch2_making_recommendations/core.clj
+++ ch2-making-recommendations/src/ch2_making_recommendations/core.clj
@@ -1,5 +1,10 @@
 (ns ch2-making-recommendations.core
-  [:require clojure.set])
+  (:require [clojure.set]
+            [clojure.math.numeric-tower :as math]))
+
+(defn- squares-of-differences
+  [prefs people movie]
+  (math/expt (- (get-in prefs [(first people) movie]) (get-in prefs [(second people) movie])) 2))
 
 (defn sim-distance
   "Determines how similar people are in their tastes. You do this by comparing
@@ -10,6 +15,9 @@
                            (fn [dict movie] (assoc dict movie 1)) 
                            {}
                            (first (apply clojure.set/intersection 
-                             (map (fn [person] (hash-set (keys (get prefs person)))) people))))]
-    sim-liked-movies)
+                             (map (fn [person] (hash-set (keys (get prefs person)))) people))))
+        sum-of-squares   (reduce 
+                           + 
+                           (map #(squares-of-differences prefs people %) (keys sim-liked-movies)))]
+    sum-of-squares)
 )
