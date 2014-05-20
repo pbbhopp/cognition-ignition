baba03996e8ace5bda0c60a42c4327d00dc843be
Thane Vo <vothane@gmail.com>
2014-05-19 18:47:00 -0600

    add TDD test for Pearson score

--- ch3-discovering-groups/test/ch3_discovering_groups/core_test.clj
+++ ch3-discovering-groups/test/ch3_discovering_groups/core_test.clj
@@ -2,8 +2,12 @@
   (:require [clojure.test :refer :all]
             [ch3-discovering-groups.data :refer :all]
             [ch3-discovering-groups.core :refer :all]))
+(def v1
+  [0.0 1.0 0.0 0.0 3.0 3.0 0.0 0.0 3.0 0.0 6.0 0.0 1.0 0.0 4.0 3.0 0.0 0.0 0.0 0.0 0.0 4.0 0.0])
+(def v2 
+  [0.0 2.0 1.0 0.0 6.0 2.0 1.0 0.0 4.0 5.0 25.0 0.0 0.0 0.0 6.0 12.0 4.0 2.0 1.0 4.0 0.0 3.0 0.0])
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest pearson-test
+  (testing "Pearson score"
+    (is (= (pearson v1 v2) 0.250049252619))))
 
