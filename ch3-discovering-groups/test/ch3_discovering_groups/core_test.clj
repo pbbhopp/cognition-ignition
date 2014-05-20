d525e4b4bc84a3543e36d6d7eaec0620dd663175
Thane Vo <vothane@gmail.com>
2014-05-19 19:30:27 -0600

    add second temporary test for second stage calc in pearson

--- ch3-discovering-groups/test/ch3_discovering_groups/core_test.clj
+++ ch3-discovering-groups/test/ch3_discovering_groups/core_test.clj
@@ -10,5 +10,5 @@
 (deftest pearson-test
   (testing "Pearson score"
     ;(is (= (pearson v1 v2) 0.250049252619)
-    (is (= (pearson v1 v2) 165.04347826086956))))
+    (is (= (pearson v1 v2) 220.072423206))))
 
