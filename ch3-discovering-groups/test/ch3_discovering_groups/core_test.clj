07404d2e0100259a6cd765bc24cbfebaff08076e
Thane Vo <vothane@gmail.com>
2014-05-19 18:22:56 -0600

    add data for TDD tests

--- ch3-discovering-groups/test/ch3_discovering_groups/core_test.clj
+++ ch3-discovering-groups/test/ch3_discovering_groups/core_test.clj
@@ -1,7 +1,9 @@
 (ns ch3-discovering-groups.core-test
   (:require [clojure.test :refer :all]
+            [ch3-discovering-groups.data :refer :all]
             [ch3-discovering-groups.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
