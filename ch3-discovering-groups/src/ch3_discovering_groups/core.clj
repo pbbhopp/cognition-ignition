61dccfdc1d0206f4b20c75f442b85143f9a5bc0c
Thane Vo <vothane@gmail.com>
2014-05-23 12:22:40 -0600

    implement code to pass first temp TDD test for kmeans clustering

--- ch3-discovering-groups/src/ch3_discovering_groups/core.clj
+++ ch3-discovering-groups/src/ch3_discovering_groups/core.clj
@@ -30,3 +30,20 @@
          den     (if (pos? d) d 0)]
     (- 1.0 (/ num den))))
 
+(defn find-by [f lazy-coll]
+  (reduce #(conj %1 (apply f (doall %2))) [] lazy-coll))
+
+(defn kmeans
+  "The K-means algorithm will determine the size of the clusters based on the
+   structure of the data. K-means clustering begins with k randomly placed
+   centroids (points in space that represent the center of the cluster), and
+   assigns every item to the nearest one. After the assignment, the centroids
+   are moved to the average location of all the nodes assigned to them, and
+   the assignments are redone. This process repeats until the assignments stop
+   changing."
+  [rows & {:keys [k] :or {k 2}}]
+  (let [nrows     (count rows)
+        ncols     (count (first rows))
+        col-group (partition nrows (apply interleave rows))
+        ranges    (split-at ncols (interleave (find-by min col-group) (find-by max col-group)))]
+    ranges))
--- ch3-discovering-groups/test/ch3_discovering_groups/core_test.clj
+++ ch3-discovering-groups/test/ch3_discovering_groups/core_test.clj
@@ -1,38 +1,13 @@
 (ns ch3-discovering-groups.core-test
   (:require [clojure.test :refer :all]
-            [ch3-discovering-groups.core :refer :all])
-  (:use     [clojure.string :as str]
-            [clojure.data.csv :only (read-csv)]
-            [clojure.java.io :as io]))
-
-(defn make-blog-data []
-  {:words [] :blogs [] :word-counts []})
-
-(defn strings->nums [coll]
-  (map read-string coll))
-
-(defn append-data [blog-data line]
-  (let [blog-name  (first line)
-        counts     (strings->nums (rest line))]
-    (merge-with concat blog-data {:blogs [blog-name] :word-counts counts})))
-
-(defn make-data [file]
-  (let [raw   (with-open [in-file (clojure.java.io/reader file)]
-                (doall (clojure.data.csv/read-csv in-file)))
-        head  (first raw)
-        tally (rest raw)
-        data  (reduce
-                append-data
-                (make-blog-data)
-                tally)]
-    (assoc data :words head)))
-
-(def data (make-data "test/ch3_discovering_groups/data.csv"))
+            [ch3-discovering-groups.core :refer :all]))
 
 (def v1
   [0.0 1.0 0.0 0.0 3.0 3.0 0.0 0.0 3.0 0.0 6.0 0.0 1.0 0.0 4.0 3.0 0.0 0.0 0.0 0.0 0.0 4.0 0.0])
-(def v2 
+(def v2
   [0.0 2.0 1.0 0.0 6.0 2.0 1.0 0.0 4.0 5.0 25.0 0.0 0.0 0.0 6.0 12.0 4.0 2.0 1.0 4.0 0.0 3.0 0.0])
+
+(def k-data [[1.0 1.0] [1.5 2.0] [3.0 4.0] [5.0 7.0] [3.5 5.0] [4.5 5.0] [3.5 4.5]])
 
 (deftest pearson-test
   (testing "Pearson score"
@@ -40,4 +15,5 @@
 
 (deftest kmeans-test
   (testing "kmeans clustering"
-    (is (= (take 5 (kmeans (:word-counts data))) [0.7408868896992121 0.31305486463589927 3.992773202381228 20.445880868103867 15.259185101093632]))))
+    (is (= (kmeans k-data)
+           [[1.0 5.0] [1.0 7.0]]))))
