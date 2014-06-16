f5396442e95bc41b081c37920a42f9e69d0820b2
Thane Vo <vothane@gmail.com>
2014-06-16 13:02:23 -0600

    setup ch4 neural networks

---
+++ ch4-neural-networks/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ch4-neural-networks
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ ch4-neural-networks/test/ch4_neural_networks/core_test.clj
@@ -0,0 +1,7 @@
+(ns ch4-neural-networks.core-test
+  (:require [clojure.test :refer :all]
+            [ch4-neural-networks.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
