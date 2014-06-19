2a39dbd00858a59506acff063a8c4befa0ce2a82
Thane Vo <vothane@gmail.com>
2014-06-19 14:42:53 -0600

    make neural-network an atom

--- ch4-neural-networks/src/ch4_neural_networks/core.clj
+++ ch4-neural-networks/src/ch4_neural_networks/core.clj
@@ -10,29 +10,29 @@
   (vec (take rows (repeat (repeat-vector cols val-fn)))))
 
 (defn make-neural-network [input-nodes hidden-nodes output-nodes]
-  {:hidden-activ (repeat-vector (+ 1 hidden-nodes) 1) 
-   :input-activ  (repeat-vector (+ 1 input-nodes) 1)
-   :output-activ (repeat-vector output-nodes 1) 
-   :in-weight-diff  (make-matrix input-nodes hidden-nodes #(0)) 
-   :out-weight-diff (make-matrix hidden-nodes output-nodes #(0))
-   :hidden-nodes (+ 1 hidden-nodes) 
-   :input-nodes  (+ 1 input-nodes) 
-   :output-nodes output-nodes 
-   :input-weights  (make-matrix input-nodes hidden-nodes rnd) 
-   :output-weights (make-matrix hidden-nodes output-nodes rnd)})
+  (atom {:hidden-activ (repeat-vector (+ 1 hidden-nodes) 1) 
+         :input-activ  (repeat-vector (+ 1 input-nodes) 1)
+         :output-activ (repeat-vector output-nodes 1) 
+         :in-weight-diff  (make-matrix input-nodes hidden-nodes #(0)) 
+         :out-weight-diff (make-matrix hidden-nodes output-nodes #(0))
+         :hidden-nodes (+ 1 hidden-nodes) 
+         :input-nodes  (+ 1 input-nodes) 
+         :output-nodes output-nodes 
+         :input-weights  (make-matrix input-nodes hidden-nodes rnd) 
+         :output-weights (make-matrix hidden-nodes output-nodes rnd)}))
 
 (defn activate-input [in-act input] 
   (into input (subvec in-act (count input))))
 
 (defn activate-inputs [neural-network inputs] 
-  (let [input-activations (mapv #(activate-input (:input-activ neural-network) (first %)) inputs)]
-    (assoc neural-network :input-activ input-activations)))
+  (mapv #(activate-input (:input-activ @neural-network) (first %)) inputs))
 
 (defn sigmoid [x] (Math/tanh x))
 
 (defn sigmoid-derivative [y] (- 1.0 (* y y)))
 
-(defn activate-hidden-node [input-node weight-node]
+(defn activate-node [input-node weight-node]
   (let [add-coll (partition 2 (interleave input-node weight-node))
         total    (reduce + (map #(* (first %) (second %)) add-coll))]
     (sigmoid total)))
+
--- ch4-neural-networks/test/ch4_neural_networks/core_test.clj
+++ ch4-neural-networks/test/ch4_neural_networks/core_test.clj
@@ -11,9 +11,9 @@
 (deftest make-neural-network-test
   (testing "making an initial state neural network"
     (let [nn (make-neural-network 2 2 1)]
-      (is (= (:hidden-activ nn) [1 1 1]))
-      (is (= (:input-activ nn) [1 1 1]))
-      (is (= (:output-activ nn) [1])))))
+      (is (= (:hidden-activ @nn) [1 1 1]))
+      (is (= (:input-activ @nn) [1 1 1]))
+      (is (= (:output-activ @nn) [1])))))
 
 (deftest activate-input-test
   (testing "input activation for single input node"
@@ -21,10 +21,9 @@
 
 (deftest activate-inputs-test
   (testing "input activation for many input nodes"
-    (let [nn1 (make-neural-network 2 2 1)
-          nn2 (activate-inputs nn1 training-input)]
-      (is (= (:input-activ nn2) [[0 0 1] [0 1 1] [1 0 1] [1 1 1]])))))
+    (let [nn1 (make-neural-network 2 2 1)]
+      (is (= (activate-inputs nn1 training-input) [[0 0 1] [0 1 1] [1 0 1] [1 1 1]])))))
 
-(deftest activate-hidden-node-test
-  (testing "find acivation value for a single hidden node"
-    (is (= (activate-hidden-node [5 7 9] [1 0 1]) 0.9999999999986171))))
+(deftest activate-node-test
+  (testing "find acivation value for a single node"
+    (is (= (activate-node [5 7 9] [1 0 1]) 0.9999999999986171))))
