26d17d9ef50e9be725e6a998b1aee2e1f67cda86
Thane Vo <vothane@gmail.com>
2014-06-20 00:56:54 -0600

    major fixes

--- ch4-neural-networks/src/ch4_neural_networks/core.clj
+++ ch4-neural-networks/src/ch4_neural_networks/core.clj
@@ -3,28 +3,28 @@
 (defn repeat-vector [n val-fn]
   (vec (take n (repeatedly val-fn))))
 
-(defn rnd [a b]
-  (+ (* (- b a) (rand)) a))
+(defn rnd []
+  (+ (* (- 1 -1) (rand)) -1))
 
 (defn make-matrix [rows cols val-fn]
-  (vec (take rows (repeat (repeat-vector cols val-fn)))))
+  (vec (take rows (repeatedly #(repeat-vector cols val-fn)))))
 
 (defn make-neural-network [input-nodes hidden-nodes output-nodes]
-  (atom {:hidden-activ (repeat-vector (+ 1 hidden-nodes) 1) 
-         :input-activ  (repeat-vector (+ 1 input-nodes) 1)
-         :output-activ (repeat-vector output-nodes 1) 
-         :in-weight-diff  (make-matrix input-nodes hidden-nodes #(0)) 
-         :out-weight-diff (make-matrix hidden-nodes output-nodes #(0))
-         :hidden-nodes (+ 1 hidden-nodes) 
-         :input-nodes  (+ 1 input-nodes) 
-         :output-nodes output-nodes 
-         :input-weights  (make-matrix input-nodes hidden-nodes #(rnd -1 1)) 
-         :output-weights (make-matrix hidden-nodes output-nodes #(rnd -1 1))}))
+  (atom {:hidden-activ (make-matrix 1 (+ 1 hidden-nodes) (fn [] 1))
+         :input-activ  (make-matrix 1 (+ 1 input-nodes) (fn [] 1))
+         :output-activ (make-matrix 1 output-nodes (fn [] 1))
+         :in-weight-diff  (make-matrix input-nodes hidden-nodes (fn [] 0))
+         :out-weight-diff (make-matrix hidden-nodes output-nodes (fn [] 0))
+         :hidden-nodes (+ 1 hidden-nodes)
+         :input-nodes  (+ 1 input-nodes)
+         :output-nodes output-nodes
+         :input-weights  (make-matrix input-nodes hidden-nodes rnd)
+         :output-weights (make-matrix hidden-nodes output-nodes rnd)}))
 
-(defn activate-input [in-act input] 
+(defn activate-input [in-act input]
   (into input (subvec in-act (count input))))
 
-(defn activate-inputs [neural-network inputs] 
+(defn activate-inputs [neural-network inputs]
   (mapv #(activate-input (:input-activ @neural-network) (first %)) inputs))
 
 (defn sigmoid [x] (Math/tanh x))
@@ -32,7 +32,7 @@
 (defn sigmoid-derivative [y] (- 1.0 (* y y)))
 
 (defn activate-node [input-node weight-node]
-  (let [add-coll (partition 2 (interleave input-node weight-node))
+  (let [add-coll (partition 2 (interleave (first input-node) weight-node))
         total    (reduce + (map #(* (first %) (second %)) add-coll))]
     (sigmoid total)))
 
--- ch4-neural-networks/test/ch4_neural_networks/core_test.clj
+++ ch4-neural-networks/test/ch4_neural_networks/core_test.clj
@@ -11,28 +11,23 @@
 (deftest make-neural-network-test
   (testing "making an initial state neural network"
     (let [nn (make-neural-network 2 2 1)]
-      (is (= (:hidden-activ @nn) [1 1 1]))
-      (is (= (:input-activ @nn) [1 1 1]))
-      (is (= (:output-activ @nn) [1])))))
+      (is (= (:hidden-activ @nn) [[1 1 1]]))
+      (is (= (:input-activ @nn) [[1 1 1]]))
+      (is (= (:output-activ @nn) [[1]])))))
 
-(deftest activate-input-test
-  (testing "input activation for single input node"
-    (is (= (activate-input [1 1 1] [0 0]) [0 0 1]))))
+(def wi [[ 0.6888437030500962  0.515908805880605   -0.15885683833831] 
+         [-0.4821664994140733  0.02254944273721704 -0.19013172509917142] 
+         [ 0.5675971780695452 -0.3933745478421451  -0.04680609169528838]])
 
-(deftest activate-inputs-test
-  (testing "input activation for many input nodes"
-    (let [nn1 (make-neural-network 2 2 1)]
-      (is (= (activate-inputs nn1 training-input) [[0 0 1] [0 1 1] [1 0 1] [1 1 1]])))))
-
-(deftest activate-node-test
-  (testing "find acivation value for a single node"
-    (is (= (activate-node [5 7 9] [1 0 1]) 0.9999999999986171))))
+(def wo [[0.1667640789100624] [0.8162257703906703] [0.009373711634780513]])
 
 (deftest update-test
   (testing "update neural network for given training inputs"
-    (let [nn (make-neural-network 2 2 1)
-           _ (println @nn)]
+    (let [nn (make-neural-network 2 2 1)]
       (update nn training-input)
-      (is (= (:input-activ @nn) [0 0 1]))
-      (is (= (:hidden-activ @nn) [0.5135924495204157 -0.37426574501728027 1.0]))
-      (is (= (:output-activ @nn) [-0.2074094931212734])))))
+      (swap! nn assoc :input-weights wi)
+      (swap! nn assoc :output-weights wo)
+      (println @nn)
+      (is (= (:input-activ @nn) [[0 0 1]]))
+      (is (= (:hidden-activ @nn) [[0.5135924495204157 -0.37426574501728027 1.0]]))
+      (is (= (:output-activ @nn) [[-0.2074094931212734]])))))
