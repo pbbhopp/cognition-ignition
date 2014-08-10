5153ec62c55b2f54a506655edbed27e7819586c7
Thane Vo <vothane@gmail.com>
2014-08-10 15:23:08 -0600

    add code for counting words in training string

--- ch6-naive-bayes/src/ch6_naive_bayes/core.clj
+++ ch6-naive-bayes/src/ch6_naive_bayes/core.clj
@@ -1,6 +1,21 @@
 (ns ch6-naive-bayes.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defprotocol Filter
+  (increment-feature [data-set feature category])
+  (increment-category [data-set category])
+  (category-count [data-set category])
+  (feature-count [data-set feature category]))
+
+(extend-type clojure.lang.PersistentHashSet
+  Filter
+  (increment-feature [data-set feature category]
+    (let [result  (clojure.set/select #(= (:word %) feature) data-set)
+          updated (merge-with + {:count 1 :c 1} (:counts (first result)))]
+      updated)))
+
+(defprotocol Feature
+  (get-words [str]))
+
+(extend-type String
+  Feature
+  (get-words [str] (apply hash-set (clojure.string/split str #" "))))
