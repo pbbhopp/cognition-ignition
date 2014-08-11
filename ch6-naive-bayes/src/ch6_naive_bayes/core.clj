574550273931560c90f5bed5740da8ac0b661bf5
Thane Vo <vothane@gmail.com>
2014-08-11 01:30:23 -0600

    add implementation for training classifier

--- ch6-naive-bayes/src/ch6_naive_bayes/core.clj
+++ ch6-naive-bayes/src/ch6_naive_bayes/core.clj
@@ -1,25 +1,17 @@
 (ns ch6-naive-bayes.core)
 
-(defprotocol Filter
-  (increment-feature [data feature category])
-  (increment-category [data category])
-  (category-count [data category])
-  (feature-count [data feature category])
-  (feature-probability [data feature category]))
+(defn increment-feature [data feature category]
+  (let [result  ((keyword feature) @data {category 0})
+        updated (merge-with + {(keyword category) 1} result)]
+    (swap! data assoc (keyword feature) updated)))
 
-(extend-type clojure.lang.PersistentArrayMap
-  Filter
-  (increment-feature [data feature category]
-    (let [result  ((keyword feature) data)
-          updated (merge-with + {(keyword category) 1} result)]
-      (assoc data (keyword feature) updated)))
-  (feature-probability [data feature category]
-    (let [result ((keyword feature) data)
-          sum    (reduce + (vals result))
-          count  (category result)]             
-      (if (= count 0)
-        0
-        (/ count sum)))))
+(defn feature-probability [data feature category]
+  (let [result ((keyword feature) data)
+        sum    (reduce + (vals result))
+        count  (category result)]             
+    (if (= count 0)
+      0
+      (/ count sum))))
 
 (defprotocol Feature
   (get-words [str]))
@@ -27,3 +19,7 @@
 (extend-type String
   Feature
   (get-words [str] (apply hash-set (clojure.string/split str #" "))))
+
+(defn train [classifier terms category]
+  (doseq [word terms]
+    (increment-feature classifier word category)))
--- ch6-naive-bayes/test/ch6_naive_bayes/core_test.clj
+++ ch6-naive-bayes/test/ch6_naive_bayes/core_test.clj
@@ -2,9 +2,16 @@
   (:require [clojure.test :refer :all]
             [ch6-naive-bayes.core :refer :all]))
 
-(deftest feature-test
-  (testing "feature increment and probability"
-    (let [counter (increment-feature {:rabbit {:good 2 :bad 3}} "rabbit" :good)
-          prob    (feature-probability {:rabbit {:good 2 :bad 3}} "rabbit" :good)]
-    (is (= counter {:rabbit {:good 3 :bad 3}}))
-    (is (= prob (/ 2 5))))))
+(def classifier (atom {}))
+
+(train classifier (get-words "Nobody owns the water") :good)
+(train classifier (get-words "the quick rabbit jumps fences") :good)
+(train classifier (get-words "buy pharmaceuticals now") :bad)
+(train classifier (get-words "make quick money at the online casino") :bad)
+(train classifier (get-words "the quick brown fox jumps") :good)
+(println @classifier)
+
+(deftest feature-count-test
+  (testing "should track feature counts correctly"
+    (is (= (:quick @classifier) {:good 2 :bad 1}))
+    (is (= (:jumps @classifier) {:good 2}))))
