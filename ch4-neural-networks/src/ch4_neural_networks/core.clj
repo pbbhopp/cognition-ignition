6d3c5186e607ce3bf721e14d3aebb5814a8a7b8a
Thane Vo <vothane@gmail.com>
2014-10-31 00:25:29 +0000

    implement and test for recursive forward propagate neural network

--- ch4-neural-networks/src/ch4_neural_networks/core.clj
+++ ch4-neural-networks/src/ch4_neural_networks/core.clj
@@ -1,11 +1,39 @@
 (ns ch4-neural-networks.core
   (:require [clojure.math.numeric-tower :as math]))
 
-(defn repeat-vector [n val-fn]
-  (vec (take n (repeatedly val-fn))))
-
 (defn rnd [] (+ (* (- 1 -1) (rand)) -1))
 
 (defn sigmoid [x] (Math/tanh x))
 
 (defn dsigmoid [y] (- 1.0 (* y y)))
+
+(defn make-network
+  [rows cols f]
+  (vec (repeat rows (vec (repeat cols f)))))
+
+(defn make-layer [num-nodes num-inputs]
+  {:weights    (make-network num-nodes num-inputs rnd)
+   :last-delta (make-network num-nodes num-inputs (fn [] 0))
+   :deriv      (make-network num-nodes num-inputs (fn [] 0))})
+
+(defn interleave-multiply [& colls]
+  (let [coll (partition (count colls) (apply interleave colls))]
+    (map #(reduce * %) coll)))
+
+(defn activate [weights input]
+  (let [init-sum (* (last weights) 1)
+        mults    (interleave-multiply (drop-last weights) input)
+        sum      (reduce + mults)]
+    (+ init-sum sum)))
+
+(defn forward 
+  ([weights input]
+    (let [active (activate (first weights) input)
+          output (sigmoid active)]
+    (forward (rest weights) input (vector active) (vector output))))
+  ([weights input activations outputs]
+    (if (empty? weights)
+      {:activations activations :outputs outputs}
+      (let [active (activate (first weights) input)
+            output (sigmoid active)]
+        (forward (rest weights) input (conj activations active) (conj outputs output))))))
--- ch4-neural-networks/test/ch4_neural_networks/core_test.clj
+++ ch4-neural-networks/test/ch4_neural_networks/core_test.clj
@@ -2,8 +2,16 @@
   (:require [clojure.test :refer :all]
             [ch4-neural-networks.core :refer :all]))
 
-(deftest make-neural-network-test
-  (testing "making an initial state neural network"
-    (let [nn [(make-layer 4 2) (make-layer 1 2)]]
-      (is (= (count nn) 2)))))
+(def weights [[ 0.014375367932126654  0.216311916893445420  0.74139912430793380] 
+               [-0.245757173379700660 -0.267149242897378800  0.15417318630866406]
+               [-0.145751006387052960 -0.024118668071382188 -0.13222709112239060]
+               [ 0.224316313871827040 -0.183629891976191730 -0.40702572197133036]])
+
+(deftest recursive-forward-propagate-test
+  (testing "recursively forward propagate neural network"
+    (let [results     (forward weights '(0))
+          activations (:activations results)
+          outputs     (:outputs results)]
+      (is (= activations [0.7413991243079338 0.15417318630866406 -0.1322270911223906 -0.276425077965549]))
+      (is (= outputs [0.6299897352646393 0.15296315642714714 -0.13146182301041381 -0.2695931916334675])))))
 
