6d0a02c6cad39e35f2be67237f354a83b14bb1e2
Thane Vo <vothane@gmail.com>
2015-02-13 01:51:31 -0700

    add comment on where (url) test result values can be cross-validated

--- ch4-neural-networks/test/ch4_neural_networks/core_test.clj
+++ ch4-neural-networks/test/ch4_neural_networks/core_test.clj
@@ -1,6 +1,10 @@
 (ns ch4-neural-networks.core-test
   (:require [clojure.test :refer :all]
             [ch4-neural-networks.core :refer :all]))
+; 
+; Test case values were taken from http://www.generation5.org/content/2001/xornet.asp.
+; You can go there to cross-validate test results below.
+;
 
 (deftest forward-propagate-test
   (testing "should forward propagate neural network with correct outputs and activations"
