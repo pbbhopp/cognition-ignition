753c80c763d9effc1c7707d45e8f98a735c81c9d
Thane Vo <vothane@gmail.com>
2015-02-13 01:34:58 -0700

    use simpler data structures for neural network thus less complex and shorter code. SIMPLE IS GOOD -> KISS

--- ch4-neural-networks/src/ch4_neural_networks/core.clj
+++ ch4-neural-networks/src/ch4_neural_networks/core.clj
@@ -1,84 +1,39 @@
 (ns ch4-neural-networks.core
   (:require [clojure.math.numeric-tower :as math]))
 
-(defn rnd [] (+ (* (- 1 -1) (rand)) -1))
+(defn rnd [] (+ (* (- 1.0 -1.0) (rand)) -1.0))
 
-(defn sigmoid [x] (Math/tanh x))
+(defn sigmoid [x] (/ 1.0 (+ 1.0 (Math/exp (* -1.0 x)))))
 
-(defn dsigmoid [y] (- 1.0 (* y y)))
+(defn dsigmoid [y] (* y (- 1.0 y)))
 
-(defn make-network [rows cols f]
-  (vec (repeat rows (vec (repeat cols f)))))
+(defn make-vector-with [n f]
+  (take n (repeatedly f)))
 
-(defn make-layer [num-nodes num-inputs]
-  {:weights    (make-network num-nodes num-inputs rnd)
-   :last-delta (make-network num-nodes num-inputs (fn [] 0))
-   :deriv      (make-network num-nodes num-inputs (fn [] 0))})
-
-(defn interleave-multiply [& colls]
-  (let [coll (partition (count colls) (apply interleave colls))]
-    (map #(reduce * %) coll)))
+(defn make-neuron [num-inputs]
+  {:weights    (make-vector-with num-inputs rnd)
+   :last-delta (make-vector-with num-inputs (fn [] 0))
+   :deriv      (make-vector-with num-inputs (fn [] 0))})
 
 (defn activate [weights input]
-  (let [init-sum (* (last weights) 1)
-        mults    (interleave-multiply (drop-last weights) input)
-        sum      (reduce + mults)]
-    (+ init-sum sum)))
+  (let [sum (reduce + (map #(* %1 %2) (drop-last weights) input))]
+   (+ (* (last weights) 1) sum)))
 
-(defn forward 
-  ([weights input]
-    (let [active (activate (first weights) input)
-          output (sigmoid active)]
-      (forward (rest weights) input (vector active) (vector output))))
-  ([weights input activations outputs]
-    (if (empty? weights)
-      {:activations activations :outputs outputs}
-      (let [active (activate (first weights) input)
-            output (sigmoid active)]
-        (forward (rest weights) input (conj activations active) (conj outputs output))))))
+(defn activate-neuron [neuron input]
+  (let [n  (assoc neuron :activation (activate (:weights neuron) input))
+        -n (assoc n :output (sigmoid (:activation n)))]
+    -n))
 
-(defn forward-layer [network idx-weights input]
-  (let [[idx weights] idx-weights
-        -input        (if (zero? idx) input (get-in network [(dec idx) :outputs]))
-        result        (forward weights -input)
-        -network      (assoc-in network [idx :activations] (:activations result))]
-    (assoc-in -network [idx :outputs] (:outputs result))))
-
-(defn forward-propagate [network input]
-  (let [idx-weights-coll (map-indexed #(vector %1 (:weights %2)) network)]
-    (reduce #(forward-layer %1 %2 input) network idx-weights-coll)))
-
-(defn output-error [network expected-output]
-  (let [output (first (:outputs (last network)))
-        error  (- expected-output output)
-        delta  (* error (dsigmoid output))]
-    (assoc-in network [(dec (count network)) :deltas] [delta])))
-
-(defn transpose [coll]
-  (apply map vector coll))
-
-(defn back-prop-layer [network idx-layer parted-net]
-  (let [layer->  (first parted-net)
-        ->layer  (second parted-net)
-        sum-errs (map #(reduce + (interleave-multiply % (:deltas ->layer))) (transpose (:weights ->layer)))
-        deltas   (interleave-multiply sum-errs (map dsigmoid (:outputs layer->)))]
-    (assoc-in network [idx-layer :deltas] (vec deltas))))
-
-(defn backward-propagate [network expected-output]
-  (let [-network   (output-error network expected-output)
-        parted-net (vec (reverse (partition 2 1 -network)))]
-    (reduce-kv #(back-prop-layer %1 %2 %3) -network parted-net)))
-
-(defn err-deriv [deriv inputs delta]
-  (let [-deriv (mapv #(+ %1 (* %2 delta)) (drop-last deriv) inputs)] 
-    (conj -deriv (+ (last deriv) (* delta 1.0)))))
-
-(defn err-deriv-layer [network idx-layer _ inputs]
-  (let [-inputs (if (zero? idx-layer) inputs (get-in network [(dec idx-layer) :outputs]))
-        derivs  (get-in network [idx-layer :derivs])
-        -derivs (map-indexed #(err-deriv %2 -inputs (get-in network [idx-layer :deltas %1])) derivs)]
-    (assoc-in network [idx-layer :derivs] (vec -derivs))))
-
-(defn error-derivatives [network inputs]
-  (let [idx-layers (vec (range (count network)))]
-    (reduce-kv #(err-deriv-layer %1 %2 %3 inputs) network idx-layers)))
+(defn forward-propagate
+  ([network input]
+    (let [layer (map #(activate-neuron % input) (first network))
+          input (map :output layer)
+          net   (conj [] layer)]
+      (forward-propagate (rest network) input net)))
+  ([layers input net]
+    (if (empty? layers)
+      net
+      (let [layer (map #(activate-neuron % input) (first layers))
+            input (map :output layer)
+            net   (conj net layer)]
+        (forward-propagate (rest layers) input net)))))
--- ch4-neural-networks/test/ch4_neural_networks/core_test.clj
+++ ch4-neural-networks/test/ch4_neural_networks/core_test.clj
@@ -2,69 +2,19 @@
   (:require [clojure.test :refer :all]
             [ch4-neural-networks.core :refer :all]))
 
-(def weights [[ 0.014375367932126654  0.216311916893445420  0.74139912430793380] 
-               [-0.245757173379700660 -0.267149242897378800  0.15417318630866406]
-               [-0.145751006387052960 -0.024118668071382188 -0.13222709112239060]
-               [ 0.224316313871827040 -0.183629891976191730 -0.40702572197133036]])
-
-(deftest recursive-forward-propagate-test
-  (testing "recursively forward propagate neural network"
-    (let [results     (forward weights [0 0])
-          activations (:activations results)
-          outputs     (:outputs results)]
-      (is (= activations [0.7413991243079338 0.15417318630866406 -0.1322270911223906 -0.40702572197133036]))
-      (is (= outputs [0.6299897352646393 0.15296315642714714 -0.13146182301041381 -0.38594433967392094])))))
-
-(defn make-net []
-  [{:weights [[0.014375367932126654 0.21631191689344542 0.7413991243079338]
-              [-0.24575717337970066 -0.2671492428973788 0.15417318630866406]
-              [-0.14575100638705296 -0.024118668071382188 -0.1322270911223906]
-              [0.22431631387182704 -0.18362989197619173 -0.40702572197133036]]
-    :last-delta [[0.0 0.0 0.0]
-                 [0.0 0.0 0.0]
-                 [0.0 0.0 0.0]
-                 [0.0 0.0 0.0]]
-    :derivs [[0.0 0.0 0.0]
-             [0.0 0.0 0.0]
-             [0.0 0.0 0.0]
-             [0.0 0.0 0.0]]}          
-   {:weights [[0.19869154577851822 0.06557770702187848 0.25770916351234513 -0.30487838476232276 -0.4954168557933254]]
-    :last-delta [[0.0 0.0 0.0 0.0 0.0]]
-    :derivs [[0.0 0.0 0.0 0.0 0.0]]}])
-
 (deftest forward-propagate-test
-  (testing "should forward propagate neural network with coreect outputs and activations"
-    (let [nn       (make-net)
-          -nn      (forward-propagate nn [0 0])
-          outputs1 (:outputs (first -nn))
-          activs1  (:activations (first -nn))
-          outputs2 (:outputs (second -nn))
-          activs2  (:activations (second -nn))]
-      (is (= outputs1 [0.6299897352646393 0.15296315642714714 -0.13146182301041381 -0.38594433967392094]))
-      (is (= activs1 [0.7413991243079338 0.15417318630866406 -0.1322270911223906 -0.40702572197133036]))
-      (is (= outputs2 [-0.26959319163346745]))
-      (is (= activs2 [-0.27642507796554894])))))
+  (testing "should forward propagate neural network with correct outputs and activations"
+    (let [nn [[{:weights [0.129952 -0.923123  0.341232] :last-delta [0 0] :deriv [0 0]} 
+               {:weights [0.570345 -0.328932 -0.115223] :last-delta [0 0] :deriv [0 0]}] 
+              [{:weights [0.164732  0.752621 -0.993423] :last-delta [0 0] :deriv [0 0]}]]
+          nn (forward-propagate nn [0 0])
+          n1 (ffirst nn)
+          n2 (second (first nn))
+          n3 (first (second nn))]
+      (is (=  0.3412320000000000 (:activation n1)))
+      (is (=  0.5844897593895427 (:output n1)))
+      (is (= -0.1152230000000000 (:activation n2)))
+      (is (=  0.4712260773225621 (:output n2)))
+      (is (= -0.5424841914156577 (:activation n3))) 
+      (is (=  0.3676098854895219 (:output n3))))))
 
-(deftest backward-propagate-test
-  (testing "should backward propagate neural network with correct deltas"
-    (let [nn (-> (make-net)
-                 (forward-propagate [0 0])
-                 (backward-propagate 0))
-          deltas1 (:deltas (first nn))
-          deltas2 (:deltas (last nn))]
-      (is (= deltas1 [0.029958243616168813 0.01601077209310136 0.06331359834046521 -0.06486620358573723]))
-      (is (= deltas2 [0.24999902664118395])))))
-   
-(deftest error-derivatives-test
-  (testing "should correctly calculate error derivatives after back propagation"
-    (let [nn (-> (make-net)
-                 (forward-propagate [0 0])
-                 (backward-propagate 0)
-                 (error-derivatives [0 0]))
-          derivs1 (:derivs (first nn))
-          derivs2 (:derivs (last nn))]
-      (is (= derivs1 [[0.0  0.0  0.029958243616168813]
-                      [0.0  0.0  0.01601077209310136]
-                      [0.0  0.0  0.06331359834046521]
-                      [0.0  0.0 -0.06486620358573723]]))
-      (is (= derivs2 [[0.15749682061009698 0.03824064021874995 -0.032865327793079054 -0.09648570925615471 0.24999902664118395]])))))   
