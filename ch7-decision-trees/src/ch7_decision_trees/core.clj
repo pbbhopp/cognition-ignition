ae46cd9edf02a9ac2c6fe3a6b9ddfa0246e32b96
Thane Vo <vothane@gmail.com>
2015-02-15 23:53:58 -0700

    iterate only through unique features otherwise redundant

--- ch7-decision-trees/src/ch7_decision_trees/core.clj
+++ ch7-decision-trees/src/ch7_decision_trees/core.clj
@@ -20,12 +20,12 @@
     
 (defn find-best-feature-to-split [data-set]
   (let [entropy    (shannon-entropy data-set)
-        feat-list  (distinct (drop-last (transpose data-set)))
+        feat-list  (drop-last (transpose data-set))
         fn-prob    (fn [idx value] 
                      (let [sub-data-set (split-data-set data-set idx value)]
                        (* (/ (count sub-data-set) (count data-set)) (shannon-entropy sub-data-set))))
         fn-entrop  (fn [idx feats] 
-                     (reduce #(+ %1 (fn-prob idx %2)) 0.0 feats))
+                     (reduce #(+ %1 (fn-prob idx %2)) 0.0 (distinct feats)))
         info-gains (map-indexed #(vector %1 (- entropy (fn-entrop %1 %2))) feat-list)
         best-feat  (first (last (sort-by second info-gains)))]
     best-feat)) 
