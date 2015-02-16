8425f900232044af231856e8315175e35d7bff67
Thane Vo <vothane@gmail.com>
2015-02-15 23:14:16 -0700

    implement and test find-best-feature-to-split function

--- ch7-decision-trees/src/ch7_decision_trees/core.clj
+++ ch7-decision-trees/src/ch7_decision_trees/core.clj
@@ -14,3 +14,18 @@
   (let [data (filter #(= (get % axis) value) data-set)
         data (map #(concat (subvec % 0 axis) (subvec % (inc axis))) data)]
     data))  
+
+(defn transpose [coll]
+  (apply map vector coll))
+    
+(defn find-best-feature-to-split [data-set]
+  (let [entropy    (shannon-entropy data-set)
+        feat-list  (drop-last (transpose data-set))
+        fn-prob    (fn [idx value] 
+                     (let [sub-data-set (split-data-set data-set idx value)]
+                       (* (/ (count sub-data-set) (count data-set)) (shannon-entropy sub-data-set))))
+        fn-entrop  (fn [idx feats] 
+                     (reduce #(+ %1 (fn-prob idx %2)) 0.0 feats))
+        info-gains (map-indexed #(vector %1 (- entropy (fn-entrop %1 %2))) feat-list)
+        best-feat  (first (last (sort-by second info-gains)))]
+    best-feat)) 
--- ch7-decision-trees/test/ch7_decision_trees/core_test.clj
+++ ch7-decision-trees/test/ch7_decision_trees/core_test.clj
@@ -1,6 +1,10 @@
 (ns ch7-decision-trees.core-test
   (:require [clojure.test :refer :all]
             [ch7-decision-trees.core :refer :all]))
+;
+; this example was taken from the book "Machine Learning in Action" from Manning publications
+; chapter 3 pages 39-48
+;
 
 (deftest a-test
   (testing "Shannon entropy"
@@ -12,3 +16,8 @@
     (let [data [[1 1 "yes"] [1 1 "yes"] [1 0 "no"] [0 1 "no"] [0 1 "no"]]]
       (is (= (split-data-set data 0 1) [[1 "yes"] [1 "yes"] [0 "no"]]))
       (is (= (split-data-set data 0 0) [[1 "no"] [1 "no"]])))))
+
+(deftest best-feature-split-test
+  (testing "best feature split"
+    (let [data [[1 1 "yes"] [1 1 "yes"] [1 0 "no"] [0 1 "no"] [0 1 "no"]]]
+      (is (= (find-best-feature-to-split data) 0)))))
