a8e5b81fe08ed8f6c492c0ba931c025eb81c1572
Thane Vo <vothane@gmail.com>
2015-02-18 01:12:00 -0700

    save work

--- ch7-decision-trees/src/ch7_decision_trees/core.clj
+++ ch7-decision-trees/src/ch7_decision_trees/core.clj
@@ -14,21 +14,37 @@
 
 (defn split-data-set [data-set axis value]
   (let [data (filter #(= (get % axis) value) data-set)
-        data (map #(del % axis) data)]
-    data))  
+        data (map #(del (vec %) axis) data)]
+    data))
 
 (defn transpose [coll]
   (apply map vector coll))
-    
+
 (defn find-best-feature-to-split [data-set]
   (let [entropy    (shannon-entropy data-set)
         feat-list  (drop-last (transpose data-set))
-        fn-prob    (fn [idx value] 
+        fn-prob    (fn [idx value]
                      (let [sub-data-set (split-data-set data-set idx value)]
                        (* (/ (count sub-data-set) (count data-set)) (shannon-entropy sub-data-set))))
-        fn-entrop  (fn [idx feats] 
+        fn-entrop  (fn [idx feats]
                      (reduce #(+ %1 (fn-prob idx %2)) 0.0 (distinct feats)))
         info-gains (map-indexed #(vector %1 (- entropy (fn-entrop %1 %2))) feat-list)
         best-feat  (first (last (sort-by second info-gains)))]
-    best-feat)) 
+    best-feat))
+
+(defn build-tree [data-set labels]
+  (let [classes (last (transpose data-set))]
+    (cond
+      (= (count (distinct classes)) 1) (first classes)
+      (= (count (first data-set)) 1) (first (last (sort-by second (frequencies (into [] classes)))))
+      :else
+        (let [best-feat (find-best-feature-to-split data-set)
+              best-lbl  (get labels best-feat)
+              dec-tree  (hash-map best-lbl {})
+              features  (distinct (for [data data-set] (get data best-feat)))
+              dec-tree  (for [feat features]
+                          (assoc-in dec-tree [best-lbl feat]
+                            (build-tree
+                              (split-data-set data-set best-feat feat) (del (vec labels) best-feat))))]
+          dec-tree))))
 
--- ch7-decision-trees/test/ch7_decision_trees/core_test.clj
+++ ch7-decision-trees/test/ch7_decision_trees/core_test.clj
@@ -6,18 +6,25 @@
 ; chapter 3 pages 39-48
 ;
 
-(deftest a-test
-  (testing "Shannon entropy"
+(deftest entropy-test
+  (testing "calculating Shannon entropy"
     (let [data [[1 1 "yes"] [1 1 "yes"] [1 0 "no"] [0 1 "no"] [0 1 "no"]]]
       (is (= (shannon-entropy data) 0.9709505944546686)))))
 
 (deftest split-test
-  (testing "split"
+  (testing "split data set (vector of vectors) on axis"
     (let [data [[1 1 "yes"] [1 1 "yes"] [1 0 "no"] [0 1 "no"] [0 1 "no"]]]
       (is (= (split-data-set data 0 1) [[1 "yes"] [1 "yes"] [0 "no"]]))
       (is (= (split-data-set data 0 0) [[1 "no"] [1 "no"]])))))
 
 (deftest best-feature-split-test
-  (testing "best feature split"
+  (testing "best feature split on axis that gives best info gain"
     (let [data [[1 1 "yes"] [1 1 "yes"] [1 0 "no"] [0 1 "no"] [0 1 "no"]]]
       (is (= (find-best-feature-to-split data) 0)))))
+
+(deftest build-decision-tree-test
+  (testing "building decision btree"
+    (let [data-set [[1 1 "yes"] [1 1 "yes"] [1 0 "no"] [0 1 "no"] [0 1 "no"]]
+          feature-labels ["no surfacing" "flippers"]
+          tree (build-tree data-set feature-labels)]
+      (is (= tree {"no surfacing" {0 "no" 1 {"flippers" {0 "no" 1 "yes"}}}})))))
