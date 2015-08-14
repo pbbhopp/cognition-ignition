2af1d8d71e62d9016f638107705a3ea61bab1acc
vothane <vothane@gmail.com>
2015-08-14 03:37:53 +0000

    add backprop fn with test

--- ch4-neural-networks/src/ch4_neural_networks/layer.clj
+++ ch4-neural-networks/src/ch4_neural_networks/layer.clj
@@ -3,3 +3,7 @@
 (defn feed [w x f]
   (let [coll (map #(reduce + (map (fn [a b] (* a b)) %1 %2)) w x)]
     (map f coll)))
+
+(defn backprop [w v s df]
+  (let [err (map #(* %1 %2) s (map df v))]
+    (map #(reduce + (map * err %)) w)))
--- ch4-neural-networks/test/ch4_neural_networks/layer_test.clj
+++ ch4-neural-networks/test/ch4_neural_networks/layer_test.clj
@@ -4,8 +4,17 @@
 
 (defn sigmoid [x] (/ 1.0 (+ 1.0 (Math/exp (* -1.0 x)))))
 
+(defn dsigmoid [y] (* y (- 1.0 y)))
+
 (deftest feed-test
   (testing "should feed"
     (let [w [[1 1] [2 2]]
           x [[1 1] [1 1]]]
       (is (= (feed w x sigmoid) (map sigmoid '(2 4)))))))
+
+(deftest backprop-test
+  (testing "should backprop"
+    (let [w [[1 1] [2 2]]
+          v [2 2]
+          s [1 1]]
+	  (is (= (backprop w v s dsigmoid) '(-4.0 -8.0))))))
