b0cb71863fdf9608a88d382c6f572b4ae9183935
vothane <vothane@gmail.com>
2015-08-14 03:00:24 +0000

    add function parameter to feed fn

--- ch4-neural-networks/src/ch4_neural_networks/layer.clj
+++ ch4-neural-networks/src/ch4_neural_networks/layer.clj
@@ -1,4 +1,5 @@
 (ns ch4-neural-networks.layer)
   
-(defn feed [w x]
-  (map #(reduce + (map (fn [a b] (* a b)) %1 %2)) w x))
+(defn feed [w x f]
+  (let [coll (map #(reduce + (map (fn [a b] (* a b)) %1 %2)) w x)]
+    (map f coll)))
--- ch4-neural-networks/test/ch4_neural_networks/layer_test.clj
+++ ch4-neural-networks/test/ch4_neural_networks/layer_test.clj
@@ -2,8 +2,10 @@
   (:require [clojure.test :refer :all]
             [ch4-neural-networks.layer :refer :all]))
 
+(defn sigmoid [x] (/ 1.0 (+ 1.0 (Math/exp (* -1.0 x)))))
+
 (deftest feed-test
   (testing "should feed"
     (let [w [[1 1] [2 2]]
           x [[1 1] [1 1]]]
-      (is (= (feed w x) '(2 4))))))
+      (is (= (feed w x sigmoid) (map sigmoid '(2 4)))))))
