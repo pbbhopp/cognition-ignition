372089ae695fd36fb311afc6f062a795d34df934
vothane <vothane@gmail.com>
2015-08-15 02:28:31 +0000

    define Layer domain model

--- ch4-neural-networks/src/ch4_neural_networks/layer.clj
+++ ch4-neural-networks/src/ch4_neural_networks/layer.clj
@@ -1,5 +1,16 @@
 (ns ch4-neural-networks.layer)
   
+(defrecord Layer [weights activations errors])
+
+(defn rnd [] (+ (* (- 1.0 -1.0) (rand)) -1.0))
+
+(defn make-vector [n f] (into [] (take n (repeatedly f))))
+
+(defn make-layer [n m]
+  (->Layer (into [] (take n (repeatedly (partial make-vector m rnd))))
+           (make-vector n (fn [] 0.0))
+           (make-vector n (fn [] 0.0))))
+           
 (defn feed [w x f]
   (let [coll (map #(reduce + (map (fn [a b] (* a b)) %1 %2)) w x)]
     (map f coll)))
@@ -7,3 +18,4 @@
 (defn backprop [w v s df]
   (let [err (map #(* %1 %2) s (map df v))]
     (map #(reduce + (map * err %)) w)))
+
--- ch4-neural-networks/test/ch4_neural_networks/layer_test.clj
+++ ch4-neural-networks/test/ch4_neural_networks/layer_test.clj
@@ -18,3 +18,14 @@
           v [2 2]
           s [1 1]]
 	  (is (= (backprop w v s dsigmoid) '(-4.0 -8.0))))))
+
+(deftest make-layer-test
+  (testing "should make layer"
+    (let [l (make-layer 2 2)
+          w (:weights l)]
+      (is (= (instance? clojure.lang.PersistentVector w)))
+      (is (= (instance? clojure.lang.PersistentVector (first w))))
+      (is (= (count w) 2))
+      (is (= (count (first w)) 2))
+      (is (= (:activations l) [0.0 0.0]))
+      (is (= (:errors l) [0.0 0.0])))))
