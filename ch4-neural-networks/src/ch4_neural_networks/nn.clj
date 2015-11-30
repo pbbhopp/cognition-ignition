b3c61958adf32c48515a5fc4c90ec2e1a6703f87
Thane Vo <vothane@gmail.com>
2015-11-30 23:34:13 +0000

    fix so that the resulted neural network with updated weights has the same structure as original

--- ch4-neural-networks/src/ch4_neural_networks/nn.clj
+++ ch4-neural-networks/src/ch4_neural_networks/nn.clj
@@ -15,18 +15,18 @@
 
 (defn calc-deltas [nn outputs actuals df]
   (let [output-deltas (map #(* (df %1) (- %1 %2)) (last outputs) actuals)
-        hidden-deltas (map #(* (df %1) (dot (first outputs) %2)) output-deltas (last nn))]
-    (vector (vec hidden-deltas) (vec output-deltas))))
+        hidden-deltas (map-indexed #(* (df %2) (dot output-deltas (for [n (last nn)] (get n %1)))) (first outputs))]
+    (vector hidden-deltas output-deltas)))
 
 (defmulti update-weights (fn [layer idx inputs outputs deltas] (if (zero? idx) :first :last)))
 (defmethod update-weights :last [layer _ _ outputs deltas]
   (let [deltas  (transpose (last deltas))
         outputs (transpose (concat (first outputs) [1]))]
-    (mapv #(mapv (fn [n d o] (- n (* (first d) (first o)))) %1 deltas outputs) layer)))
+    (mapv #(mapv (fn [n o] (- n (* (first %1) (first o)))) %2 outputs) deltas layer)))
 (defmethod update-weights :first [layer _ inputs _ deltas]
   (let [deltas (transpose (first deltas))
         inputs (transpose (concat inputs [1]))]
-    (mapv #(mapv (fn [n d i] (- n (* (first d) (first i)))) %1 deltas inputs) layer)))
+    (mapv #(mapv (fn [n i] (- n (* (first %1) (first i)))) %2 inputs) deltas layer)))
 
 (defn backward-propagate [nn inputs actuals f df]
   (let [indices (range (count nn))
