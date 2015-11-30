c95f6b4ffe50a664edf0978846ef0af8c6ebfd0c
Thane Vo <vothane@gmail.com>
2015-11-30 06:40:41 +0000

    simplify backward-propagate fn with less state variables and use functional abstractions instead

--- ch4-neural-networks/src/ch4_neural_networks/nn.clj
+++ ch4-neural-networks/src/ch4_neural_networks/nn.clj
@@ -3,62 +3,33 @@
 
 (defn dot [v w] (reduce + (map * v w)))
 
+(defn transpose [m] (mapv vector m))
+
 (defn activate [weights input-vector activate-fn] (activate-fn (dot weights input-vector)))
 
 (defn forward-propagate [nn input-vector activate-fn]
   (let [activate-fn (fn [outputs layer]
-                      (let [biased-input (conj (last outputs) 1)]
+                      (let [biased-input (concat (last outputs) [1])]
                         (for [neuron layer] (activate neuron biased-input activate-fn))))]
     (rest (reduce #(conj %1 (activate-fn %1 %2)) (vector input-vector) nn))))
 
-(defmulti update-layer (fn [nn idx ex df] (if (= (last nn) (get nn idx)) :last :other)))
+(defn calc-deltas [nn outputs actuals df]
+  (let [output-deltas (map #(* (df %1) (- %1 %2)) (last outputs) actuals)
+        hidden-deltas (map #(* (df %1) (dot (first outputs) %2)) output-deltas (last nn))]
+    (vector (vec hidden-deltas) (vec output-deltas))))
 
-(defmethod update-layer :last [nn idx ex df]
-  (let [out (get-in nn [idx 0 :output])
-        err (- ex out)]
-    (assoc-in nn [idx 0 :delta] (* err (df out)))))
+(defmulti update-weights (fn [layer idx inputs outputs deltas] (if (zero? idx) :first :last)))
+(defmethod update-weights :last [layer _ _ outputs deltas]
+  (let [deltas  (transpose (last deltas))
+        outputs (transpose (concat (first outputs) [1]))]
+    (mapv #(mapv (fn [n d o] (- n (* (first d) (first o)))) %1 deltas outputs) layer)))
+(defmethod update-weights :first [layer _ inputs _ deltas]
+  (let [deltas (transpose (first deltas))
+        inputs (transpose (concat inputs [1]))]
+    (mapv #(mapv (fn [n d i] (- n (* (first d) (first i)))) %1 deltas inputs) layer)))
 
-(defmethod update-layer :other [nn idx _ df]
-  (let [m (fn [layer k] (map #(* (get-in % [:weights k]) (get % :delta)) layer))
-        s (fn [nn k] (reduce + (m (get nn (inc idx)) k)))
-        f (fn [nn k] (assoc-in nn [idx k :delta] (* (s nn k) (df (get-in nn [idx k :output])))))]
-    (reduce #(f %1 %2) nn (range (count (get nn idx))))))
-
-(defn backward-propagate [nn expected-output df]
-  (let [index (reverse (range (count nn)))]
-    (reduce #(update-layer %1 %2 expected-output df) nn index)))
-
-(defn calc-err-derivatives [nn input-vector]
-  (let [inputs  (concat (vector input-vector) (mapv #(mapv (fn [n] (:output n)) %) (drop-last nn)))
-        inputs  (mapv #(concat % [1]) inputs)
-        deriv-f (fn [deriv delta input] (mapv + (map #(* % delta) input) deriv))
-        assoc-f (fn [nn l-idx n-idx]
-                  (let [neuron (get-in nn [l-idx n-idx])]
-                    (assoc-in nn [l-idx n-idx :deriv]
-                              (deriv-f (:deriv neuron) (:delta neuron) (get inputs l-idx)))))
-        layer-f (fn [nn l-idx] (reduce #(assoc-f %1 l-idx %2) nn (range (count (get nn l-idx)))))]
-    (reduce #(layer-f %1 %2) nn (range (count nn)))))
-
-(defn update-weights [nn lr m]
-  (let [delta-f (fn [neuron]
-                  (map + (map #(* lr %) (:deriv neuron)) (map #(* m %) (:last-delta neuron))))
-        assoc-f (fn [nn l-idx n-idx]
-                  (let [neuron (get-in nn [l-idx n-idx])
-                        delta  (delta-f neuron)]
-                    (-> nn
-                        (assoc-in [l-idx n-idx :weights] (mapv + (:weights neuron) delta))
-                        (assoc-in [l-idx n-idx :last-delta] delta)
-                        (assoc-in [l-idx n-idx :deriv] 0))))
-        layer-f (fn [nn l-idx] (reduce #(assoc-f %1 l-idx %2) nn (range (count (get nn l-idx)))))]
-    (reduce #(layer-f %1 %2) nn (range (count nn)))))
-
-(defn train-network [nn domain lrate mom f df]
-  (let [input (vec (drop-last domain))
-        expected (get domain 2)
-        f  (fn [net input]
-             (let [_   (forward-propagate net input f)
-                   net (backward-propagate net expected df)]
-               (calc-err-derivatives nn input)))
-        nn (reduce #(f %1 %2) nn domain)]
-    (update-weights nn lrate mom)))
-
+(defn backward-propagate [nn inputs actuals f df]
+  (let [indices (range (count nn))
+        outputs (forward-propagate nn inputs f)
+        deltas  (calc-deltas nn outputs actuals df)]
+    (mapv #(update-weights %1 %2 inputs outputs deltas) nn indices)))
